4bd5981501267d21dec413d06ec206045c11604b
Josiah <josiah.james.fowler@gmail.com>
2021-06-03 11:39:40 -0400

    longestWord

--- Beginner/longestWord/index-START.js
+++ Beginner/longestWord/index-START.js
@@ -6,7 +6,14 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 
 
 function longestWord(text) {
-    // Code goes here
+    let result = text.split(' ').reduce((maxLengthWord, currentWord) => {
+        if (currentWord.length > maxLengthWord.length) {
+            return currentWord
+        } else {
+            return maxLengthWord
+        }
+    }, "")
+    return result
 }
 
 
